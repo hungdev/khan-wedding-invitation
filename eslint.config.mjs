d582bc05065c7e11268f58365d87f6d2f9d5b88f
hungvu <hungns126@gmail.com>
2025-12-06 23:46:42 +0700

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,71 +1,18 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import prettierPlugin from 'eslint-plugin-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals.js';
+import nextTs from 'eslint-config-next/typescript.js';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const flatCompat = [
-  // 기존 next.js 타입스크립트 config
-  ...compat.extends('next/core-web-vitals'),
-  ...compat.extends('next/typescript'),
-
-  // Prettier 설정 추가
-  {
-    name: 'prettier',
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      '@next/next/no-img-element': 'off',
-    },
-  },
-  {
-    rules: {
-      semi: ['error', 'never'],
-      'comma-dangle': [
-        'error',
-        {
-          arrays: 'always-multiline',
-          objects: 'always-multiline',
-          imports: 'always-multiline',
-          exports: 'always-multiline',
-          functions: 'never',
-        },
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          semi: false,
-          singleQuote: true,
-          trailingComma: 'es5',
-        },
-      ],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          args: 'after-used',
-          ignoreRestSiblings: true,
-          varsIgnorePattern: '^_',
-          argsIgnorePattern: '^_',
-        },
-      ],
-      'react/prop-types': 'off',
-      'import/no-anonymous-default-export': [
-        'error',
-        {
-          allowObject: true,
-        },
-      ],
-    },
-  },
-]
-export default flatCompat
+export default eslintConfig;
